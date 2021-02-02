ab8d3a974c423071b05e26ed072008c94823f7d2
RianIslam <rianislamdr@gmail.com>
2021-02-02 14:17:10 +0600

    map filter

---
+++ map.js
@@ -0,0 +1,18 @@
+ const num = [2,24,4,5,6,7,8,98];
+// const output =[];
+// for (let i = 0; i<num.length;i++){
+//     const element = num[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+
+//  const result = num.map(function(element){
+//     return element * element;
+
+// })
+
+
+// const result = num.map(x => x*x);
+
+const result = num.filter(x=>x>59)
+console.log(result);
