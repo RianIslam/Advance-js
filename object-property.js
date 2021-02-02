5a5db141761f323e9211d43c70b65c9eff1f4c9e
RianIslam <rianislamdr@gmail.com>
2021-02-02 14:27:55 +0600

    object map

--- object-property.js
+++ object-property.js
@@ -1,7 +1,9 @@
 const students = [
-    {id:21 ,neme: 'rabbi'},
-    {id:31 ,neme: 'riam'},
-    {id:51 ,neme: 'karim'},
-    {id:41 ,neme: 'jody'},
-    {id:31 ,neme: 'kodu '}
+    {id:21 ,name: 'rabbi'},
+    {id:31 ,name: 'riam'},
+    {id:51 ,name: 'karim'},
+    {id:41 ,name: 'jody'},
+    {id:31 ,name: 'kodu '}
 ]
+const names = students.map(s => s.name);
+console.log(names);
